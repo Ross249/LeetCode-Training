50d7101caa2fa9746ad4fc7002519f303d80ea39
Ross249 <528266131@qq.com>
2021-01-06 21:44:05 +0800

    完成重复数相关练习

--- src/com/train/algorithm/sort/CyclicSort.java
+++ src/com/train/algorithm/sort/CyclicSort.java
@@ -24,4 +24,42 @@ public class CyclicSort {
         }
         return res;
     }
+
+    // leetcode-287
+    public int findDuplicate(int[] nums) {
+        int length = nums.length;
+        int left = 0,right = length - 1;
+        int res = -1;
+        while(left <= right){
+            int mid = (right + left) / 2;
+            int count = 0;
+            for (int i = 0;i < length ;i++ ) {
+                if (nums[i] <= mid) {
+                    count++;
+                }
+            }
+            if (count <= mid) {
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+                res = mid;
+            }
+        }
+        return res;
+    }
+
+    // leetcode-442
+    public List<Integer> findDuplicates(int[] nums) {
+        int len = nums.length;
+        List<Integer> res = new ArrayList<Integer>();
+        for (int i = 0;i < len ;i++ ) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] *= -1;
+            }else{
+                res.add(num);
+            }
+        }
+        return res;
+    }
 }
