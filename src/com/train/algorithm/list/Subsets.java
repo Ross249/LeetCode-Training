c7969cd6eb98297cd62ac028f464c83b3f56c654
Ross249 <528266131@qq.com>
2021-01-13 20:57:55 +0800

    补充子集练习

--- src/com/train/algorithm/list/Subsets.java
+++ src/com/train/algorithm/list/Subsets.java
@@ -1,9 +1,10 @@
 package com.train.algorithm.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-// 子集问题
+// 子集问题(回朔法)
 public class Subsets {
     // leetcode-78
     List<Integer> temp = new ArrayList<Integer>();
@@ -21,5 +22,47 @@ public class Subsets {
         dfs(cur+1,nums);
         temp.remove(temp.size() - 1);
         dfs(cur+1,nums);
+    }
+
+    // leetcode-46
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> temp = new ArrayList<Integer>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for(int num:nums){
+            temp.add(num);
+        }
+        int n = nums.length;
+        dfs(n,temp,res,0);
+        return res;
+    }
+    public void dfs(int n,List<Integer> temp,List<List<Integer>> res,int first){
+        if (first == n) {
+            res.add(new ArrayList<Integer>(temp));
+        }
+        for (int i = first;i < n ;i++ ) {
+            Collections.swap(temp,first,i);
+            dfs(n,temp,res,first+1);
+            Collections.swap(temp,first,i);
+        }
+    }
+
+    // leetcode-784
+    public List<String> letterCasePermutation(String S) {
+        List<String> res = new ArrayList<>();
+        char[] charArray = S.toCharArray();
+        dfs(charArray,0,res);
+        return res;
+    }
+    private void dfs(char[] charArray,int index,List<String> res){
+        if (index == charArray.length) {
+            res.add(new String(charArray));
+            return;
+        }
+        dfs(charArray,index+1,res);
+        if (Character.isLetter(charArray[index])) {
+            // 转大写
+            charArray[index] ^= 1 << 5;
+            dfs(charArray,index+1,res);
+        }
     }
 }
