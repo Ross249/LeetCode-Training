31f99f63337a6a1fca2c3a2fe0d52593307b30f6
Ross249 <528266131@qq.com>
2021-01-16 20:00:34 +0800

    完善多路归并相关练习

--- src/com/train/algorithm/list/KWayMerge.java
+++ src/com/train/algorithm/list/KWayMerge.java
@@ -1,5 +1,7 @@
 package com.train.algorithm.list;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 // 多路归并
@@ -44,5 +46,61 @@ public class KWayMerge {
             }
         }
         return head.next;
+    }
+
+    // leetcode-378
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b){
+                return a[0]-b[0];
+            }
+        });
+        int n = matrix.length;
+        for (int i = 0;i < n ;i++ ) {
+            queue.offer(new int[]{matrix[i][0],i,0});
+        }
+        for (int i = 0;i < k - 1 ;i++ ) {
+            int[] temp = queue.poll();
+            if (temp[2] != n - 1) {
+                queue.offer(new int[]{matrix[temp[1]][temp[2] + 1],temp[1],temp[2] + 1});
+            }
+        }
+        return queue.poll()[0];
+    }
+
+    // leetcode-632
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int left = 0,right = Integer.MAX_VALUE;
+        int min = right - left;
+        int max = Integer.MIN_VALUE;
+        int size = nums.size();
+        int[] next = new int[size];
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer index1,Integer index2){
+                return nums.get(index1).get(next[index1]) - nums.get(index2).get(next[index2]);
+            }
+        });
+        for (int i = 0;i < size ;i++ ) {
+            queue.offer(i);
+            max = Math.max(max,nums.get(i).get(0));
+        }
+        while(true){
+            int minIndex = queue.poll();
+            int cur = max - nums.get(minIndex).get(next[minIndex]);
+            if (cur < min) {
+                min = cur;
+                left = nums.get(minIndex).get(next[minIndex]);
+                right = max;
+            }
+            next[minIndex]++;
+            if (next[minIndex] == nums.get(minIndex).size()) {
+                break;
+            }
+            queue.offer(minIndex);
+            max = Math.max(max,nums.get(minIndex).get(next[minIndex]));
+        }
+        return new int[]{left,right};
     }
 }
