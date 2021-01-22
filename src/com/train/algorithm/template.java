cad66ebe8322fb7a24d6aed34f002ebcb0b42bc7
Ross249 <528266131@qq.com>
2021-01-22 19:09:32 +0800

    添加练习模版

---
+++ src/com/train/algorithm/template.java
@@ -0,0 +1,12 @@
+package com.train.algorithm;
+
+public class template {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
