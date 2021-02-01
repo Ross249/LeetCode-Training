cda57f4e34fda16da56abb30c21555463a3ae0a2
Ross249 <528266131@qq.com>
2021-02-01 20:41:46 +0800

    add some templates in medium mode

--- src/com/train/algorithm/TemplateMediumMode.java
+++ src/com/train/algorithm/TemplateMediumMode.java
@@ -27,4 +27,36 @@ public class TemplateMediumMode {
             this.right = right;
         }
     }
+
+    // NC-140
+    public static int[] MySort (int[] arr) {
+        quickSort(arr,0,arr.length-1);
+        return arr;
+    }
+
+    public static void quickSort(int[] arr,int low,int high){
+        int pivot;
+        if (low < high){
+            pivot = partition(arr,low,high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr,int low,int high){
+        int temp = arr[low];
+        while (low < high){
+            while (low < high && arr[high] >= temp){
+                high--;
+            }
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= temp){
+                low++;
+            }
+            arr[high] = arr[low];
+        }
+        arr[low] = temp;
+        return low;
+    }
+
 }
