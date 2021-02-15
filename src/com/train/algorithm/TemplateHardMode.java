92ca9e9dcb1d720d0efb3bae122707f0bc140717
Ross249 <528266131@qq.com>
2021-02-15 19:29:07 +0800

    create templates about hard mode

---
+++ src/com/train/algorithm/TemplateHardMode.java
@@ -0,0 +1,32 @@
+package com.train.algorithm;
+
+public class TemplateHardMode {
+    public class ListNode {
+        int val;
+        ListNode next = null;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
