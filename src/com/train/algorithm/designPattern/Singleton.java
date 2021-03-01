d892253700bc0148d072d18299a4b3044bfa692c
Ross249 <528266131@qq.com>
2021-03-01 14:15:12 +0800

    fix singleton template

--- src/com/train/algorithm/designPattern/Singleton.java
+++ src/com/train/algorithm/designPattern/Singleton.java
@@ -9,7 +9,9 @@ public class Singleton {
     public static Singleton getSingleton(){
         if (singleton == null){
             synchronized (Singleton.class){
-                singleton = new Singleton();
+                if (singleton == null){
+                    singleton = new Singleton();
+                }
             }
         }
         return singleton;
