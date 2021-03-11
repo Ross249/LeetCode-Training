1383d15cb175050a1a244a4c59404be15d083e1d
Ross249 <528266131@qq.com>
2021-03-11 21:09:13 +0800

    add some templates about singleton

--- src/com/train/algorithm/designPattern/Singleton.java
+++ src/com/train/algorithm/designPattern/Singleton.java
@@ -1,5 +1,6 @@
 package com.train.algorithm.designPattern;
 
+// 双锁检测
 public class Singleton {
     private volatile static Singleton singleton;
     private Singleton(){
@@ -15,5 +16,26 @@ public class Singleton {
             }
         }
         return singleton;
+    }
+}
+
+// 懒汉式
+class Singleton1{
+    private static volatile Singleton1 singleton1 = null;
+    private Singleton1(){}
+    public static synchronized Singleton1 getInstance(){
+        if (singleton1 == null){
+            singleton1 = new Singleton1();
+        }
+        return singleton1;
+    }
+}
+
+// 饿汉式
+class Singleton2{
+    private static Singleton2 singleton2 = new Singleton2();
+    private Singleton2(){}
+    public static Singleton2 getInstance(){
+        return singleton2;
     }
 }
