b2b92a76a318195253fd4f78a004a7efbf16eb46
Ross <528266131@qq.com>
2022-01-06 22:39:01 +0800

    retry sliding window in javascript

--- src/com/train/algorithm/list/implementInJS/SlidingWindow.js
+++ src/com/train/algorithm/list/implementInJS/SlidingWindow.js
@@ -18,5 +18,25 @@ var getSmallestString = function(n, k) {
 
 // leetcode-904
 var totalFruit = function(fruits) {
+	let l = 0,maxLen = 0,n = 0;
+	let arr = [fruits[l]];
 
+	for(let r = 0;r < fruits.length;r++){
+		if(!arr.includes(fruits[r])){
+			if(arr.length <= 1){
+				arr[1] = fruits[r];
+			}else{
+				l = n;
+				arr[0] = fruits[l];
+				arr[1] = fruits[r];
+			}
+		}
+		if(fruits[r] != fruits[n]){
+			n = r;
+		}
+		maxLen = Math.max(maxLen,r - l + 1);
+	}
+
+
+	return maxLen;
 };
