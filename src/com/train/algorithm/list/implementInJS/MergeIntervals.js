278e21cd65e859d0e4a4345b926825f282e691fd
Ross <528266131@qq.com>
2022-01-19 22:20:56 +0800

    retry merge intervals in javascript

--- src/com/train/algorithm/list/implementInJS/MergeIntervals.js
+++ src/com/train/algorithm/list/implementInJS/MergeIntervals.js
@@ -16,3 +16,25 @@ var merge = function(intervals) {
 	res.push(pre);
 	return res;
 };
+
+// leetcode-57
+var insert = function(intervals, newInterval) {
+	const res = [];
+	let i = 0;
+	const len = intervals.length;
+	while(i < len && intervals[i][1] < newInterval[0]){
+		res.push(intervals[i]);
+		i++;
+	}
+	while(i < len && intervals[i][0] <= newInterval[1]){
+		newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+		newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+		i++;
+	}
+	res.push(newInterval);
+	while(i < len){
+		res.push(intervals[i]);
+		i++;
+	}
+	return res;
+};
