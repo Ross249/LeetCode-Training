c0e32bef171c86a10968f0969e1a42fab5bd4d3b
Ross <528266131@qq.com>
2022-01-23 22:16:39 +0800

    retry subsets in javascript

--- src/com/train/algorithm/list/implementInJS/Subsets.js
+++ src/com/train/algorithm/list/implementInJS/Subsets.js
@@ -17,3 +17,30 @@ var subsets = function(nums) {
 	dfs(0,nums);
 	return res;
 };
+
+//leetcode-46
+var permute = function(nums) {
+	const temp = [],res = [];
+	const len = nums.length;
+	dfs(nums,len,[]);
+	return res;
+
+
+	function dfs(n,k,used){
+		if(temp.length === k){
+			res.push(temp.slice());
+			return;
+		}
+		for(let i = 0;i < k;i++){
+			if(used[i]){
+				continue;
+			}
+			temp.push(n[i]);
+			used[i] = true;
+			dfs(n,k,used);
+			temp.pop();
+			used[i] = false;
+		}
+	}
+			
+};
