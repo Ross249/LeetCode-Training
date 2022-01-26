d53b7f661255d97b1e38412c29b78195e68a6c8d
Ross <528266131@qq.com>
2022-01-26 22:15:23 +0800

    retry k-way merge in javascript

--- src/com/train/algorithm/list/implementInJS/KWayMerge.js
+++ src/com/train/algorithm/list/implementInJS/KWayMerge.js
@@ -26,3 +26,35 @@ function mergeList(l1,l2){
 	pre.next = l1 ? l1 : l2;
 	return head.next;
 }
+
+// leetcode-378
+const countMatrix = (matrix,midVal) => {
+	const n = matrix.length;
+	let count = 0;
+	let row = 0;
+	let col = n-1;
+	while(row < n && col >= 0){
+		if(midVal >= matrix[row][col]){
+			count = count + col + 1;
+			row++;
+		}else{
+			col--;
+		}
+	}
+	return count;
+}
+var kthSmallest = function(matrix, k) {
+	const n = matrix.length;
+	let low = matrix[0][0];
+	let high = matrix[n-1][n-1];
+	while(low <= high){
+		let midVal = low + ((high - low) >>> 1);
+		let count = countMatrix(matrix,midVal);
+		if(count < k){
+			low = midVal + 1;
+		}else{
+			high = midVal - 1;
+		}
+	}
+	return low;
+};
