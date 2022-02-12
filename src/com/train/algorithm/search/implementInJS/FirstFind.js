abf072c1fe00b827dd083b1f953eed5843e7b43d
Ross <528266131@qq.com>
2022-02-12 08:46:15 +0800

    finish first find in javascript

---
+++ src/com/train/algorithm/search/implementInJS/FirstFind.js
@@ -0,0 +1,12 @@
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) {
+    return null;
+  }
+  let pA = headA,
+    pB = headB;
+  while (pA !== pB) {
+    pA = pA === null ? headB : pA.next;
+    pB = pB === null ? headA : pB.next;
+  }
+  return pA;
+};
