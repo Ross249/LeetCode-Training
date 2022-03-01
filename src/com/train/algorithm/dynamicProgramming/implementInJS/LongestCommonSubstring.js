ced35fd975354049b7c31c23f06dd56a655ef72f
Ross <528266131@qq.com>
2022-03-01 08:38:58 +0800

    retry the longest common substring in javascript

--- src/com/train/algorithm/dynamicProgramming/implementInJS/LongestCommonSubstring.js
+++ src/com/train/algorithm/dynamicProgramming/implementInJS/LongestCommonSubstring.js
@@ -92,3 +92,26 @@ var longestArithSeqLength = function (nums) {
   }
   return max;
 };
+
+// leetcode-72
+var minDistance = function (word1, word2) {
+  let dp = new Array(word1.length + 1)
+    .fill(0)
+    .map(() => new Array(word2.length + 1).fill(0));
+  for (let i = 1; i <= word1.length; i++) {
+    dp[i][0] = i;
+  }
+  for (let j = 1; j <= word2.length; j++) {
+    dp[0][j] = j;
+  }
+  for (let i = 1; i <= word1.length; i++) {
+    for (let j = 1; j <= word2.length; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+      }
+    }
+  }
+  return dp[word1.length][word2.length];
+};
