532c7eba061822b756a148a9549a1477f55689f5
Ross <528266131@qq.com>
2022-03-02 09:15:54 +0800

    finish the longest common substring in javascript

--- src/com/train/algorithm/dynamicProgramming/implementInJS/LongestCommonSubstring.js
+++ src/com/train/algorithm/dynamicProgramming/implementInJS/LongestCommonSubstring.js
@@ -115,3 +115,31 @@ var minDistance = function (word1, word2) {
   }
   return dp[word1.length][word2.length];
 };
+
+// leetcode-97
+var isInterleave = function (s1, s2, s3) {
+  if (s1.length + s2.length !== s3.length) {
+    return false;
+  }
+  const memo = new Array(s1.length + 1);
+  for (let i = 0; i < memo.length; i++) {
+    memo[i] = new Array(s2.length + 1);
+  }
+  const check = (i, j, k) => {
+    if (memo[i][j] != null) {
+      return memo[i][j];
+    }
+    if (k === s3.length) {
+      return true;
+    }
+    let res = false;
+    if (i < s1.length && s1[i] === s3[k]) {
+      res = check(i + 1, j, k + 1);
+    }
+    if (j < s2.length && s2[j] === s3[k]) {
+      res = res || check(i, j + 1, k + 1);
+    }
+    return (memo[i][j] = res);
+  };
+  return check(0, 0, 0);
+};
