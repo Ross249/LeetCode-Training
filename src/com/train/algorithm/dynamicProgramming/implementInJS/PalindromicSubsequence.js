d8998906667bf4f30d87c73f45bbd4243039ba23
Ross <528266131@qq.com>
2022-03-04 10:42:52 +0800

    retry palindromic subsequence in javascript

--- src/com/train/algorithm/dynamicProgramming/implementInJS/PalindromicSubsequence.js
+++ src/com/train/algorithm/dynamicProgramming/implementInJS/PalindromicSubsequence.js
@@ -17,3 +17,22 @@ var longestPalindromeSubseq = function (s) {
   }
   return dp[0][n - 1];
 };
+
+// leetcode-5
+var longestPalindrome = function (s) {
+  let res = "";
+  for (let i = 0; i < s.length; i++) {
+    const s1 = palindrome(s, i, i);
+    const s2 = palindrome(s, i, i + 1);
+    res = res.length > s1.length ? res : s1;
+    res = res.length > s2.length ? res : s2;
+  }
+  return res;
+};
+function palindrome(s, l, r) {
+  while (l >= 0 && r < s.length && s[l] === s[r]) {
+    l--;
+    r++;
+  }
+  return s.substr(l + 1, r - l - 1);
+}
