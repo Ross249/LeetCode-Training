1eab3dce62d3a9b8ffaa0a6c0fe35c7953fea3c0
Ross <528266131@qq.com>
2022-10-19 07:43:56 +0800

    add Maximum Depth of Binary Tree

---
+++ src/com/train/algorithm/tree/implementInC++/MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 罗斯 on 19/10/2022.
+//
+
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1  + max(left, right);
+    }
+};
