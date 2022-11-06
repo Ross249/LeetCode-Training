a6511a318d2d4aee2ec69a7ef41fc6c2e2f1afb3
Ross <528266131@qq.com>
2022-11-06 08:55:03 +0800

    add Serialize and Deserialize Binary Tree

---
+++ src/com/train/algorithm/tree/implementInC++/SerializeandDeserializeBinaryTree.cpp
@@ -0,0 +1,51 @@
+//
+// Created by 罗斯 on 6/11/2022.
+//
+#include <string>
+#include <iostream>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serialize(root, out);
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserialize(in);
+    }
+private:
+    void serialize(TreeNode* root, ostringstream& out){
+        if (!root){
+            out << "# ";
+            return;
+        }
+
+        out << root->val << " ";
+        serialize(root->left, out);
+        serialize(root->right, out);
+    }
+
+    TreeNode* deserialize(istringstream& in) {
+        string val;
+        in >> val;
+        if (val == "#") return nullptr;
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserialize(in);
+        root->right = deserialize(in);
+        return root;
+    }
+};
