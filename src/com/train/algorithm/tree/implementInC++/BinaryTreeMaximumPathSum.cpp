f8a2831bb0b1293a86d2e83af71546dd5a98db0f
Ross <528266131@qq.com>
2022-11-09 07:56:32 +0800

    add Binary Tree Maximum Path Sum

---
+++ src/com/train/algorithm/tree/implementInC++/BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 罗斯 on 9/11/2022.
+//
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        if (!root) return 0;
+        int res = INT_MIN;
+        maxPathSum(root, res);
+        return res;
+    }
+private:
+    int maxPathSum(TreeNode* root, int& res) {
+        if (!root) return 0;
+        int l = max(maxPathSum(root->left, res), 0);
+        int r = max(maxPathSum(root->right, res), 0);
+        int sum = l + r + root->val;
+        res = max(sum, res);
+        return max(l, r) + root->val;
+    }
+};
