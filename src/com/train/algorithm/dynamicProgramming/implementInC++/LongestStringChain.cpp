8d1e8fc3bd82662451ecc974580e57aaa0de0198
Ross <528266131@qq.com>
2022-12-19 08:03:06 +0800

    add Longest String Chain

---
+++ src/com/train/algorithm/dynamicProgramming/implementInC++/LongestStringChain.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 罗斯 on 19/12/2022.
+//
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    static bool compare(const string &s1, const string &s2) {
+        return s1.length() < s2.length();
+    }
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(), compare);
+        unordered_map<string, int> dp;
+        int res = 0;
+        for (string w: words) {
+            for (int i = 0; i < w.length(); i++) {
+                string pre = w.substr(0, i) + w.substr(i + 1);
+                dp[w] = max(dp[w], dp.find(pre) == dp.end() ? 1 : dp[pre] + 1);
+            }
+            res = max(res, dp[w]);
+        }
+        return res;
+    }
+};
