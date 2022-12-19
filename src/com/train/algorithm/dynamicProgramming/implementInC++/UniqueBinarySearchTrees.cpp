25693229fbd3cbb0bd26d50bca3e07371650c940
Ross <528266131@qq.com>
2022-12-20 07:43:42 +0800

    add Unique Binary Search Trees

---
+++ src/com/train/algorithm/dynamicProgramming/implementInC++/UniqueBinarySearchTrees.cpp
@@ -0,0 +1,21 @@
+//
+// Created by 罗斯 on 20/12/2022.
+//
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+};
