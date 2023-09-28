2ee04ae210618c243b105d3a15015a93bcd5165c
Ross <528266131@qq.com>
2023-09-29 07:44:49 +0800

    add pyhton implement for leetcode 141

---
+++ src/com/train/algorithm/list/implementInPython/LinkedListCycle.py
@@ -0,0 +1,15 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head:ListNode)->bool:
+        fast = head
+        while fast and fast.next:
+            head = head.next
+            fast = fast.next.next
+            if head is fast:
+                return True
+
+        return False
