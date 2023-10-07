36a81d97b61558a9f09c0a759371b16d88256f03
Ross <528266131@qq.com>
2023-10-08 07:56:15 +0800

    add pyhton implement for leetcode 21

---
+++ src/com/train/algorithm/list/implementInPython/MergeTwoSortedLists.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        head = ListNode()
+        cur = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+
+        cur.next = list1 or list2
+
+        return head.next
