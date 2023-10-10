79e734b0dd281effeacaa382fb4302afd9402e34
Ross <528266131@qq.com>
2023-10-10 08:02:01 +0800

    add pyhton implement for leetcode 148

---
+++ src/com/train/algorithm/list/implementInPython/SortList.py
@@ -0,0 +1,31 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        dummy = p = ListNode(0)
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        p.next = l or r
+        return dummy.next
+
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        fast, slow = head.next, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        start = slow.next
+        slow.next = None
+        l, r = self.sortList(head), self.sortList(start)
+        return self.merge(l, r)
