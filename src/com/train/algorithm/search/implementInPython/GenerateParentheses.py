4e4ba071a312d4a9bfdcbf098ef3204d9ed5b0c8
Ross <528266131@qq.com>
2023-11-06 07:42:33 +0800

    add pyhton implement for leetcode 22

---
+++ src/com/train/algorithm/search/implementInPython/GenerateParentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        res = []
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return
+            if left < n:
+                dfs(left + 1, right, s+"(")
+
+            if right < left:
+                dfs(left, right + 1, s + ")")
+
+        dfs(0, 0, '')
+        return res
