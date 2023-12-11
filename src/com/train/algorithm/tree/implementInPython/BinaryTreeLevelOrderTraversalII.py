787337c68239b7ea64df64461ec0fdba5df7fad0
Ross <528266131@qq.com>
2023-12-12 07:24:03 +0800

    add pyhton implement for leetcode 107

---
+++ src/com/train/algorithm/tree/implementInPython/BinaryTreeLevelOrderTraversalII.py
@@ -0,0 +1,20 @@
+import collections
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        queue, res = collections.deque([(root, 0)]), []
+        while queue:
+            node, level = queue.popleft()
+            if node:
+                if len(res) < level + 1:
+                    res.insert(0,[])
+                res[-(level + 1)].append(node.val)
+                queue.append((node.left, level+1))
+                queue.append((node.right, level+1))
+        return res
