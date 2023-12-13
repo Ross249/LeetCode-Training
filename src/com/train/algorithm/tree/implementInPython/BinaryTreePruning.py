e2ba0fcb9573a6eebae250036546422fdbff2d56
Ross <528266131@qq.com>
2023-12-14 07:22:31 +0800

    add pyhton implement for leetcode 814

---
+++ src/com/train/algorithm/tree/implementInPython/BinaryTreePruning.py
@@ -0,0 +1,11 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def pruneTree(self, root):
+        if not root: return root
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        return root if root.val == 1 or root.left or root.right else None
