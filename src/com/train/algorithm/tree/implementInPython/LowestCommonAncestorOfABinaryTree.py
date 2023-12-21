f803e83c66809aebf82e77c3a6bd6adb60d809a1
Ross <528266131@qq.com>
2023-12-22 07:25:40 +0800

    add pyhton implement for leetcode 236

---
+++ src/com/train/algorithm/tree/implementInPython/LowestCommonAncestorOfABinaryTree.py
@@ -0,0 +1,13 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if any((not root, root == p, root == q)): return root
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if not l or not r: return l if l else r
+        return root
