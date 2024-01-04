e40171fc17ef46c93b60e838992a40643e198e36
Ross <528266131@qq.com>
2024-01-05 07:23:14 +0800

    add pyhton implement for leetcode 154

---
+++ src/com/train/algorithm/Divideandconquer/implementInPython/FindMinimumInRotatedSortedArrayII.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findMin(self, nums):
+        lo, hi = 0, len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) / 2
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid if nums[hi] != nums[mid] else hi - 1
+        return nums[lo]
