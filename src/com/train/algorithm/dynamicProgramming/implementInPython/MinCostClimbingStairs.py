a2e98f131977533b13de21c630baa951de3c6201
Ross <528266131@qq.com>
2024-01-09 07:24:37 +0800

    add pyhton implement for leetcode 746

---
+++ src/com/train/algorithm/dynamicProgramming/implementInPython/MinCostClimbingStairs.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        if not cost: return 0
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        if len(cost) >= 2:
+            dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = cost[i] + min(dp[i-1]  ,dp[i-2])
+        return min(dp[-1],dp[-2])
