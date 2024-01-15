a37d02df27f1f164b51eb1013f53ed668765127b
Ross <528266131@qq.com>
2024-01-16 07:25:59 +0800

    add pyhton implement for leetcode 63

---
+++ src/com/train/algorithm/dynamicProgramming/implementInPython/UniquePathsII.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        n = len(obstacleGrid)
+        m = len(obstacleGrid[0])
+        dp = [[0] * m for i in range(n)]
+
+        if obstacleGrid[0][0] == 1:
+            dp[0][0] = 0
+        else:
+            dp[0][0] = 1
+
+        for i in range(1, m):
+            if obstacleGrid[0][i] == 1:
+                dp[0][i] == 0
+            else:
+                dp[0][i] = dp[0][i-1]
+        for i in range(1, n):
+            if obstacleGrid[i][0] == 1:
+                dp[i][0] == 0
+            else:
+                dp[i][0] = dp[i-1][0]
+
+        for i in range(1,n):
+            for j in range(1,m):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[n-1][m-1]
